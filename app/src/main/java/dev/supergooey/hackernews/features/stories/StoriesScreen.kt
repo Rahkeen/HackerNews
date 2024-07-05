b988aee1af66f3a1fc462851f19fcab5d806350f
Rikin Marfatia <rikinm10@gmail.com>
2024-07-05 11:00:01 -0700

    Using state class that represents loading items and when content is loaded

--- app/src/main/java/dev/supergooey/hackernews/features/stories/StoriesScreen.kt
+++ app/src/main/java/dev/supergooey/hackernews/features/stories/StoriesScreen.kt
@@ -3,25 +3,33 @@ package dev.supergooey.hackernews.features.stories
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.heightIn
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.layout.wrapContentHeight
 import androidx.compose.foundation.layout.wrapContentWidth
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.draw.drawBehind
 import androidx.compose.ui.geometry.Offset
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.StrokeCap
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
@@ -29,7 +37,6 @@ import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import dev.supergooey.hackernews.R
-import dev.supergooey.hackernews.data.Item
 import dev.supergooey.hackernews.features.comments.CommentsDestinations
 import dev.supergooey.hackernews.ui.theme.HNOrange
 import dev.supergooey.hackernews.ui.theme.HackerNewsTheme
@@ -58,9 +65,15 @@ fun StoriesScreen(
           onClick = {
             actions(StoriesAction.SelectStory(it.id))
             navigation(
-              StoriesNavigation.GoToStory(
-                closeup = StoriesDestinations.Closeup(it.url!!)
-              )
+              if (it.url != null) {
+                StoriesNavigation.GoToStory(
+                  closeup = StoriesDestinations.Closeup(it.url)
+                )
+              } else {
+                StoriesNavigation.GoToComments(
+                  comments = CommentsDestinations.Comments(it.id)
+                )
+              }
             )
           },
           onCommentClicked = {
@@ -105,23 +118,21 @@ private fun StoriesScreenPreview() {
       modifier = Modifier.fillMaxSize(),
       state = StoriesState(
         stories = listOf(
-          Item(
+          StoryItem.Content(
             id = 1L,
             title = "Hello There",
-            by = "heyrikin",
+            author = "heyrikin",
             score = 10,
-            type = "story",
-            descendants = 0,
-            kids = emptyList()
+            commentCount = 0,
+            url = ""
           ),
-          Item(
+          StoryItem.Content(
             id = 1L,
-            title = "Nice to Meet You",
-            by = "vasant",
-            score = 5,
-            type = "story",
-            descendants = 0,
-            kids = emptyList()
+            title = "Hello There",
+            author = "heyrikin",
+            score = 10,
+            commentCount = 0,
+            url = ""
           ),
         )
       ),
@@ -136,16 +147,26 @@ private fun StoriesScreenPreview() {
 private fun StoryRowPreview() {
   HackerNewsTheme {
     StoryRow(
-      item = Item(
+      item = StoryItem.Content(
         id = 1L,
-        by = "heyrikin",
-        title = "A theory on why NIA is a terrible example of a demo application",
+        title = "Hello There",
+        author = "heyrikin",
         score = 10,
-        type = "Story",
-        url = "www.google.com",
-        descendants = 5,
-        kids = emptyList()
+        commentCount = 0,
+        url = ""
       ),
+      onClick = {},
+      onCommentClicked = {}
+    )
+  }
+}
+
+@Preview
+@Composable
+private fun StoryRowLoadingPreview() {
+  HackerNewsTheme {
+    StoryRow(
+      item = StoryItem.Loading(id = 1L),
       onClick = {},
       onCommentClicked = {}
     )
@@ -154,66 +175,107 @@ private fun StoryRowPreview() {
 
 @Composable
 fun StoryRow(
-  item: Item,
-  onClick: (Item) -> Unit,
-  onCommentClicked: (Item) -> Unit
+  item: StoryItem,
+  onClick: (StoryItem.Content) -> Unit,
+  onCommentClicked: (StoryItem.Content) -> Unit
 ) {
-  Row(
-    modifier = Modifier
-      .fillMaxWidth()
-      .heightIn(min = 80.dp)
-      .background(color = MaterialTheme.colorScheme.background)
-      .clickable {
-        onClick(item)
-      }
-      .padding(8.dp),
-    verticalAlignment = Alignment.CenterVertically,
-    horizontalArrangement = Arrangement.spacedBy(16.dp, alignment = Alignment.CenterHorizontally)
-  ) {
-    Column(
-      modifier = Modifier
-        .fillMaxHeight()
-        .weight(1f),
-      verticalArrangement = Arrangement.Center
-    ) {
-      Text(
-        text = item.title ?: "",
-        style = MaterialTheme.typography.titleSmall
-      )
-      Row(horizontalArrangement = Arrangement.spacedBy(4.dp)) {
-        Text(text = "${item.score}", style = MaterialTheme.typography.labelSmall)
-        Text(text = "•", style = MaterialTheme.typography.labelSmall)
-        Text(
-          text = item.by ?: "",
-          color = HNOrange,
-          style = MaterialTheme.typography.labelSmall,
-          fontWeight = FontWeight.Medium
-        )
+  when (item) {
+    is StoryItem.Content -> {
+      Row(
+        modifier = Modifier
+          .fillMaxWidth()
+          .heightIn(min = 80.dp)
+          .background(color = MaterialTheme.colorScheme.background)
+          .clickable {
+            onClick(item)
+          }
+          .padding(8.dp),
+        verticalAlignment = Alignment.CenterVertically,
+        horizontalArrangement = Arrangement.spacedBy(16.dp, alignment = Alignment.CenterHorizontally)
+      ) {
+        Column(
+          modifier = Modifier
+            .fillMaxHeight()
+            .weight(1f),
+          verticalArrangement = Arrangement.Center
+        ) {
+          Text(
+            text = item.title,
+            style = MaterialTheme.typography.titleSmall
+          )
+          Row(horizontalArrangement = Arrangement.spacedBy(4.dp)) {
+            Text(text = "${item.score}", style = MaterialTheme.typography.labelSmall)
+            Text(text = "•", style = MaterialTheme.typography.labelSmall)
+            Text(
+              text = item.author ,
+              color = HNOrange,
+              style = MaterialTheme.typography.labelSmall,
+              fontWeight = FontWeight.Medium
+            )
+          }
+        }
+
+        Column(
+          modifier = Modifier
+            .wrapContentWidth()
+            .fillMaxHeight()
+            .padding(horizontal = 16.dp)
+            .clickable {
+              onCommentClicked(item)
+            },
+          verticalArrangement = Arrangement.Center,
+          horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+          Icon(
+            modifier = Modifier.size(20.dp),
+            painter = painterResource(R.drawable.ic_chat),
+            tint = MaterialTheme.colorScheme.onBackground,
+            contentDescription = ""
+          )
+          Text(
+            text = "${item.commentCount}",
+            style = MaterialTheme.typography.labelSmall,
+            fontWeight = FontWeight.Medium
+          )
+        }
       }
     }
-
-    Column(
-      modifier = Modifier
-        .wrapContentWidth()
-        .fillMaxHeight()
-        .padding(horizontal = 16.dp)
-        .clickable {
-          onCommentClicked(item)
-        },
-      verticalArrangement = Arrangement.Center,
-      horizontalAlignment = Alignment.CenterHorizontally
-    ) {
-      Icon(
-        modifier = Modifier.size(20.dp),
-        painter = painterResource(R.drawable.ic_chat),
-        tint = MaterialTheme.colorScheme.onBackground,
-        contentDescription = ""
-      )
-      Text(
-        text = "${item.descendants}",
-        style = MaterialTheme.typography.labelSmall,
-        fontWeight = FontWeight.Medium
-      )
+    is StoryItem.Loading -> {
+      Row(
+        modifier = Modifier
+          .fillMaxWidth()
+          .heightIn(min = 80.dp)
+          .background(color = MaterialTheme.colorScheme.background)
+          .padding(8.dp),
+        verticalAlignment = Alignment.CenterVertically,
+        horizontalArrangement = Arrangement.spacedBy(16.dp, alignment = Alignment.CenterHorizontally)
+      ) {
+        Column(
+          modifier = Modifier
+            .wrapContentHeight()
+            .weight(1f),
+          verticalArrangement = Arrangement.spacedBy(4.dp)
+        ) {
+          Box(
+            modifier = Modifier
+              .fillMaxWidth(0.8f)
+              .height(30.dp)
+              .clip(CircleShape)
+              .background(color = Color.LightGray)
+          )
+          Box(
+            modifier = Modifier
+              .fillMaxWidth(0.45f)
+              .height(30.dp)
+              .clip(CircleShape)
+              .background(color = Color.Gray)
+          )
+          Row(horizontalArrangement = Arrangement.spacedBy(4.dp), verticalAlignment = Alignment.CenterVertically) {
+            Box(modifier = Modifier.width(30.dp).height(20.dp).clip(CircleShape).background(Color.DarkGray))
+            Box(modifier = Modifier.width(40.dp).height(20.dp).clip(CircleShape).background(HNOrange))
+          }
+        }
+      }
     }
   }
 }
